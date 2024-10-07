ec28d2c58e84d0980aa99e8b7445e48c10cf10ac
Chepman32 <antonkerch555@gmail.com>
2024-10-07 19:12:24 +0300

    graphql

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "carsnipe": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "carsnipe70333e94": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/App.jsx
@@ -0,0 +1,192 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { Amplify } from "aws-amplify";
+import AuctionPage from "./pages/AuctionPage/AuctionPage";
+import CustomHeader from "./components/CustomHeader/CustomHeader";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import awsExports from "./aws-exports";
+import { generateClient } from "aws-amplify/api";
+import { listUsers } from "./graphql/queries";
+import { createUser } from "./graphql/mutations";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CarsStore from "./pages/CarPages/CarsStore";
+import MyCars from "./pages/CarPages/MyCars";
+import { Spin } from "antd";
+import { getCurrentUser } from "aws-amplify/auth";
+import AuctionsHub from "./pages/AuctionPage/AuctionHub";
+import MyBids from "./pages/AuctionPage/MyBids";
+import MyAuctions from "./pages/AuctionPage/MyAuctions";
+import SuccessfulPayment from "./components/SuccessfulPayment";
+import PaymentError from "./components/PaymentError";
+import Store from "./pages/Store/Store";
+import ProfileEditPage from "./pages/Store/ProfileEditPage/ProfileEditPage";
+
+const client = generateClient();
+Amplify.configure(awsExports);
+
+export default function App() {
+  const [isNewUser, setIsNewUser] = useState(false);
+  const [money, setMoney] = useState();
+  const [playerInfo, setPlayerInfo] = useState(null);
+  const [email, setEmail] = useState("");
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const createNewPlayer = useCallback(
+    async (username) => {
+      try {
+        setCreatingUser(true);
+        const data = {
+          nickname: username,
+          email,
+          money: 1000,
+          bidded: [],
+          avatar: "avatar1",
+          bio: "",
+        };
+        if (isNewUser) {
+          await client.graphql({
+            query: createUser,
+            variables: { input: data },
+          });
+        }
+        sessionStorage.setItem("signedInCars", "true");
+      } catch (error) {
+        console.error("Error creating new player:", error);
+      } finally {
+        setCreatingUser(false);
+        setLoading(false);
+      }
+    },
+    [email, isNewUser]
+  );
+
+  const currentAuthenticatedUser = useCallback(async () => {
+    try {
+      const { username, userId, signInDetails } = await getCurrentUser();
+      console.log("username", username)
+      setEmail(signInDetails.loginId);
+      const playersData = await client.graphql({
+        query: listUsers,
+      });
+      const playersList = playersData.data.listUsers.items;
+      const user = playersList.find((u) => u.email === signInDetails.loginId);
+      const isNewUser = !playersList.some((pl) => pl.email === email);
+      setIsNewUser(isNewUser);
+      if (!user) {
+        createNewPlayer(username);
+      } else {
+        setPlayerInfo(user);
+        console.log("user:", user)
+        setMoney(user.money);
+        setLoading(false);
+      }
+    } catch (err) {
+      console.error("Error fetching current authenticated user:", err);
+      setLoading(false);
+    }
+  }, [createNewPlayer, email]);
+
+  useEffect(() => {
+    currentAuthenticatedUser();
+    document.title = "Virtual cars";
+  }, [currentAuthenticatedUser]);
+
+  if (loading || creatingUser) {
+    return <Spin fullscreen />;
+  }
+
+  return (
+    <BrowserRouter>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <>
+            {playerInfo && (
+              <main>
+                <CustomHeader
+                  money={money}
+                  username={playerInfo.nickname}
+                  email={email}
+                  nickname={playerInfo.nickname}
+                  avatar={playerInfo.avatar}
+                />
+                <Routes>
+                <Route
+                    path="/profileEditPage"
+                    element={
+                      <ProfileEditPage
+                        playerInfo={playerInfo}
+                        currentAuthenticatedUser={currentAuthenticatedUser}
+                        signOut={signOut}
+                      />
+                    }
+                  />
+                  <Route
+                    path="/carsStore"
+                    element={
+                      <CarsStore
+                        playerInfo={playerInfo}
+                        money={money}
+                        setMoney={setMoney}
+                      />
+                    }
+                  />
+                  <Route
+                    path="/auctions"
+                    element={
+                      <AuctionPage
+                        playerInfo={playerInfo}
+                        money={money}
+                        setMoney={setMoney}
+                      />
+                    }
+                  />
+                  <Route
+                    path="/myCars"
+                    element={
+                      <MyCars
+                        playerInfo={playerInfo}
+                        money={money}
+                        setMoney={setMoney}
+                      />
+                    }
+                  />
+                  <Route path="/auctionsHub" element={<AuctionsHub />} />
+                  <Route
+                    path="/myBids"
+                    element={
+                      <MyBids
+                        playerInfo={playerInfo}
+                        money={money}
+                        setMoney={setMoney}
+                      />
+                    }
+                  />
+                  <Route
+                    path="/myAuctions"
+                    element={
+                      <MyAuctions
+                        playerInfo={playerInfo}
+                        money={money}
+                        setMoney={setMoney}
+                      />
+                    }
+                  />
+                  <Route
+                    path="/successfulPayment"
+                    element={<SuccessfulPayment playerInfo={playerInfo} />}
+                  />
+                  <Route path="/paymentError" element={<PaymentError />} />
+                  <Route
+                    path="/store"
+                    element={<Store email={playerInfo.email} />}
+                  />
+                </Routes>
+              </main>
+            )}
+          </>
+        )}
+      </Authenticator>
+    </BrowserRouter>
+  );
+}
