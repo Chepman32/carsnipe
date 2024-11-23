ce4beb61eff490b27c59256e97f54f8437e4afde
Chepman32 <antonkerch555@gmail.com>
2024-11-23 09:40:17 +0300

    Windows installer

--- preload.js
+++ preload.js
@@ -1,5 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('api', {
-  quitApp: () => ipcRenderer.send('quit-app'),
+contextBridge.exposeInMainWorld('electron', {
+  send: (channel, data) => {
+    ipcRenderer.send(channel, data);
+  },
+  on: (channel, callback) => {
+    ipcRenderer.on(channel, (event, ...args) => callback(...args));
+  },
 });
